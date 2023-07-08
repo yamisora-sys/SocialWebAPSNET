8fed22e290b01ddacc8e5b3dfa47676e5df89b06
yamisora-sys <20520930@ms.uit.edu.vn>
2023-07-08 14:26:40 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
